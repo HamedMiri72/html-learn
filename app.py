2cb9febfac96df258e77d543ed3e85ea05b7dd5c
HamedMiri72 <hamedmiri72@gmail.com>
2024-02-21 17:26:43 +0330

    bache kuni

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
 print("Hello world")
+
+
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+xlxs_file = "file.xlsx"
+df = pd.read_excel(xlxs_file)
+
+engine = create_engine
+df.to.sql("tabke_name", con = engine, if_exists = "replace", index = false)
+
+print("data write in to the data base")
+
+
+
+
